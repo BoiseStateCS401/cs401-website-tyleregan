f3d2432c95db1d79ad0e60eabf2eee2dbdee322e
tyleregan <TylerEgan@u.boisestate.edu>
2017-10-12 07:57:50 -0600

    Cleanup to actually finish php templating homework

--- public/BamHome.php
+++ public/BamHome.php
@@ -1,34 +1,6 @@
 <?php
-/*<!doctype html>
-<html>
 
-  <head>
-
-    <title>BAM Forums&copy;</title>
-    <link href="css/BamStyle.css" rel="stylesheet" type="text/css" />
-    <link rel="icon" type="image/png" href="images/BAM.png">
-  </head>
-
-  <body>
-    <aside class="Profile">
-      <nav>
-	    <p><a href="BamLogin.php">Login</a> &nbsp;
-	       <a href="BamSignUp.php">Sign Up</a>
-        </p>
-      </nav>
-    </aside>
-  
-    <header>
-      <p>
-        Boise state
-      <br>
-        Activities and
-      <br>
-        Majors
-      </p>
-    </header>*/
-
-  $thisPage = "BamRules";
+  $thisPage = "BamHome";
   require_once('BamHead.php');
   require_once('BamNavBar.php');
   require_once('BamHeader.php');
@@ -42,11 +14,8 @@
       <img src="images/BamMajors.jpg" width="40%" height="30%"></a> &ensp;
     </section>
 
-<?php    /*<footer>BAM Forums&copy;</footer>
-
-  </body>
-</html>*/
-require_once('BamFooter.php');
+<?php
+    require_once('BamFooter.php');
 ?>
 
 
