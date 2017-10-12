973cac9d239645188d01cc3c480d5030a2f0df8e
tyleregan <TylerEgan@u.boisestate.edu>
2017-10-12 08:03:10 -0600

    Cleanup to actually finish php templating homework

--- public/BamParkourTechniques.php
+++ public/BamParkourTechniques.php
@@ -1,32 +1,5 @@
 <?php
-/*<!doctype html>
-<html>
 
-  <head>
-
-    <title>BAM Forums&copy;</title>
-    <link href="css/BamStyle.css" rel="stylesheet" type="text/css" />
-    <link rel="icon" type="image/png" href="images/BAM.png">
-  </head>
-
-  <body>
-    <aside class="Profile">
-      <nav>
-	    <p><a href="BamLogin.php">Login</a> &nbsp;
-	       <a href="BamSignUp.php">Sign Up</a>
-        </p>
-      </nav>
-    </aside>
-  
-    <header>
-      <p>
-        Boise state
-      <br>
-        Activities and
-      <br>
-        Majors
-      </p>
-    </header>*/
   $thisPage = "BamRules";
   require_once('BamHead.php');
   require_once('BamNavBar.php');
@@ -62,10 +35,9 @@
       </ul>
     </section>
 	
-     <footer>BAM Forums&copy;</footer>
-
-  </body>
-</html>
+<?php
+    require_once('BamFooter.php');
+?>
 
 
 
