68afa7ccbf13f5c1e115e455435c4258ebe17f91
tyleregan <TylerEgan@u.boisestate.edu>
2017-10-26 15:07:58 -0600

    Capitalization Fix

--- public/BamLogin.php
+++ public/BamLogin.php
@@ -15,12 +15,12 @@
 	  <fieldset>
 	  <p>
 	    <label for="email"> Email:</label>
-	    <input type="email" name="Email" maxlength="75" value="<?= $_SESSION['presets']['email'] ?>" >
+	    <input type="email" name="email" maxlength="75" value="<?= $_SESSION['presets']['email'] ?>" >
 		<!--TODO javascript window pop up for rules on email-->
 	  </p>
 	  <p>
 	    <label for="password"> Password: </label>
-	    <input type="password" name="Password">
+	    <input type="password" name="password">
 		<!--TODO javascript window pop up for rules on password-->
 	  </p>
 	  <input type="submit" value="Login">
